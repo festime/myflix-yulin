3997a05232a5f6f503a0a2ba130b06e621dc5849
Yulin Chen <gloverock2@gmail.com>
2015-03-03 19:42:08 +0800

    Finish pre-course

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+anime = Category.create(name: "Anime")
+Category.create(name: "Movie")
+
+Video.create(title: "Attack On Titan", description: "The top 1 anime in 2013", small_cover_url: "/tmp/attack_on_titan.jpg", large_cover_url: "/tmp/attack_on_titan_large.jpg", category: anime)
+Video.create(title: "Grande Road", description: "A passionate anime", small_cover_url: "/tmp/grande_road.jpg", category: anime)
+Video.create(title: "Tokyo Ghoul", description: "Amazing...", small_cover_url: "/tmp/tokyo_ghoul.jpg", category: anime)
+Video.create(title: "Kiseji", description: "The best anime in 2014", small_cover_url: "/tmp/kiseji.jpg", category: anime)
