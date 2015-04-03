98e4ee6eb0888e2777101e80c642dd77747e2a6b
Yulin Chen <gloverock2@gmail.com>
2015-04-03 15:54:08 +0800

    Change the settings about log level and email sending in staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,6 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => 'user_name',
     :password => 'password',
@@ -27,7 +28,7 @@ Myflix::Application.configure do
     :authentication => :cram_md5
   }
 
-  ActionMailer::Base.delivery_method = :smtp
-
   config.action_mailer.default_url_options = {:host => "myflix-yulin-staging.herokuapp.com"}
+
+  config.log_level = :debug
 end
