f461500d8dab2df6b5113c89ed163b3cd9a8f89c
Yulin Chen <gloverock2@gmail.com>
2015-04-12 10:14:47 +0800

    Remove some unnecessary comments in uploader files; add a condition to .gitignore; edit carrierwave.rb to make it a little better

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,22 @@
 CarrierWave.configure do |config|
-  unless Rails.env.test?
+  if Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+  else
+    config.storage = :fog
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region:                ENV['AWS_REGION']
     }
-  end
 
-  if Rails.env.test?
-    config.storage = :file
-    config.enable_processing = false
-  else
-    config.storage = :fog
-  end
-
-  if Rails.env.production?
-    config.fog_directory  = ENV['AWS_S3_BUCKET_PRODUCTION']
-  elsif Rails.env.staging?
-    config.fog_directory  = ENV['AWS_S3_BUCKET_STAGING']
-  elsif Rails.env.development?
-    config.fog_directory  = ENV['AWS_S3_BUCKET_DEVELOPMENT']
+    if Rails.env.production?
+      config.fog_directory  = ENV['AWS_S3_BUCKET_PRODUCTION']
+    elsif Rails.env.staging?
+      config.fog_directory  = ENV['AWS_S3_BUCKET_STAGING']
+    elsif Rails.env.development?
+      config.fog_directory  = ENV['AWS_S3_BUCKET_DEVELOPMENT']
+    end
   end
 end
